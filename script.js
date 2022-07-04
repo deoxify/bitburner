936cb3460067f9746c476871c88064b4fa6e1bc8
deo <53539012+deoxify@users.noreply.github.com>
2022-07-04 07:14:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-/**
-* @param {NS} ns
-**/
 export async function main(ns) {
     var target = ns.args[0];
-    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
-    if (ns.fileExists("BruteSSH.exe", "home")) {
-        ns.brutessh(target);
-    }
-    ns.nuke(target);
+    var moneyThreshold = ns.getServerMaxMoney(target) * 0.75;
+    var securityThreshold = ns.getServerMinSecurityLevel(target) + 3;
+    
     while(true) {
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
-            await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            await ns.grow(target);
-        } else {
-            await ns.hack(target);
-        }
+        if (ns.getServerSecurityLevel(target) > securityThreshold) await ns.weaken(target);
+        else if (ns.getServerMoneyAvailable(target) < moneyThreshold) await ns.grow(target);
+        else await ns.hack(target);
     }
 }
